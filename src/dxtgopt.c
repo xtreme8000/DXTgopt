be8b28c8e6f96707bab4d1ca2ae851f860c090e3
xtreme8000 <xtreme8000@users.noreply.github.com>
2023-08-09 12:15:45 +0200

    Add license header

--- src/dxtgopt.c
+++ src/dxtgopt.c
@@ -1,3 +1,22 @@
+/*
+	Copyright (c) 2023 ByteBit/xtreme8000
+
+	This file is part of DXTgopt.
+
+	DXTgopt is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	DXTgopt is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DXTgopt.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <assert.h>
 #include <limits.h>
 #include <math.h>
